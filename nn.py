9de6b85075db67b16d90c3d179eea6b36673c4aa
JarekWnuk <j.p.wnuk@gmail.com>
2025-05-04 19:16:23 +0100

    Adds function to save model, hyperparams and metrics.

--- nn.py
+++ nn.py
@@ -1,17 +1,18 @@
-from torcheval.metrics import R2Score
+from torcheval.metrics.functional import r2_score
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from torch.utils.data import random_split
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim import *
-import torch
+import datetime
 import json
 import numpy as np
+import os
 import pandas as pd
 import tabular_data
+import torch
 import yaml
-import datetime
 
 
 class AirbnbNightlyPriceRegressionDataset(Dataset):
@@ -83,7 +84,6 @@ def evaluate(model, data_loader):
     model.eval()
     total_loss = 0
     total_pred_time = 0
-    metric = R2Score()
     with torch.no_grad():
         for batch in data_loader:
             features, label = batch
@@ -95,27 +95,42 @@ def evaluate(model, data_loader):
             if total_pred_time == 0:
                 total_pred_time = pred_duration
             total_pred_time += pred_duration
+
             loss = F.mse_loss(prediction, label)
             RMSE_loss = torch.sqrt(loss)
             total_loss += RMSE_loss.item()
-            metric.update(prediction, label)
-            r2_score = metric.compute()
+            r2 = r2_score(prediction, label)
+
     avg_pred_time = total_pred_time / len(data_loader)
     avg_RMSE_loss = total_loss / len(data_loader)
     writer.add_scalar("Loss/evaluate", avg_RMSE_loss, epoch)
     model.train()
-    return avg_RMSE_loss, r2_score, avg_pred_time
+    return avg_RMSE_loss, r2, avg_pred_time
 
 def get_nn_config():
     with open("nn_config.yaml", "r") as cf:
         nn_config_loaded = yaml.safe_load(cf)
     return nn_config_loaded
 
-def save_model(model):
+def save_model(model, model_metrics):
     if hasattr(model, "state_dict"):
-        torch.save(model.state_dict(), "model.pt")
-        f = open("hyperparameters.json", "wb")
-        f.write(model.get_config())
+        current_datetime = str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        save_path = "models/neural_networks/regression/" + current_datetime 
+        os.makedirs(save_path)
+        normalized_save_path = os.path.normcase(save_path)
+        torch.save(model.state_dict(), normalized_save_path + "/model.pt")
+        # f = open(normalized_save_path + "/hyperparameters.json", "wb")
+        # f.write(model.get_config())
+        # f2 = open(normalized_save_path + "/metrics.json", "wb")
+        # f2.write(model_metrics)
+        hyperparams = model.get_config()
+        hyperparams_path = normalized_save_path + "/hyperparameters.json"
+        with open(hyperparams_path, "w") as file:
+            json.dump(hyperparams, file)
+
+        metrics_path = normalized_save_path + "/metrics.json"
+        with open(metrics_path, "w") as file:
+            json.dump(model_metrics, file)
 
 
 if __name__ == "__main__":
@@ -123,21 +138,34 @@ if __name__ == "__main__":
     dataset = AirbnbNightlyPriceRegressionDataset()
     train_dataset, test_dataset = random_split(dataset, [0.7, 0.3])
     test_dataset, val_dataset = random_split(test_dataset, [0.5, 0.5])
+
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
     val_loader = DataLoader(val_dataset,batch_size=32, shuffle=False)
+
+    model_metrics = {}
     nn_config = get_nn_config()
     model = NN(nn_config)
     num_epochs = 50
+
     training_start_time = datetime.datetime.now()
     for epoch in range(num_epochs):
         train(model, train_loader, nn_config)
         writer.flush()
     training_end_time = datetime.datetime.now()
     training_duration = training_end_time - training_start_time
-    print(f"Training duration: {training_duration}")
+
     train_RMSE_loss, train_r2_score, avg_train_pred_time = evaluate(model, train_loader)
     val_RMSE_loss, val_r2_score, avg_val_pred_time = evaluate(model, val_loader)
+    test_RMSE_loss, test_r2_score, avg_test_pred_time = evaluate(model, val_loader)
+
+    inference_latency = (avg_train_pred_time + avg_val_pred_time + avg_test_pred_time) / 3
+    model_metrics["training_duration"] = str(training_duration)
+    model_metrics["RMSE_loss"] = [train_RMSE_loss, val_RMSE_loss, test_RMSE_loss]
+    model_metrics["R_squared"] = [float(train_r2_score), float(val_r2_score), float(test_r2_score)]
+    model_metrics["inference_latency"] = str(inference_latency)
+
+    save_model(model, model_metrics)
+
     print(f"Average validation loss: {round(val_RMSE_loss, 0)}, R2: {val_r2_score}")
     print(f"Average train loss: {round(val_RMSE_loss, 0)}, R2: {val_r2_score}")
-    print(f"Train pred time: {avg_train_pred_time}. Val pred time: {avg_val_pred_time}")
